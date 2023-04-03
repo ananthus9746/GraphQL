17c13b534ef81fee8ebe4ecfa4f123ed5a909b26
ananthus9746 <ananthus.ann@gmail.com>
2023-04-03 15:44:07 +0530

    relation databse query basic done

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const {projects,clients} = require('../sampleData')
-const {GraphQLObjectType, GraphQLScalarType, GraphQLID,GraphQLString, GraphQLSchema}=require('graphql');
+const {GraphQLObjectType, GraphQLScalarType, GraphQLID,GraphQLString, GraphQLSchema, GraphQLList}=require('graphql');
 
 const ClientType=new GraphQLObjectType({
     name:'Client',
@@ -11,18 +11,62 @@ const ClientType=new GraphQLObjectType({
     })
 })
 
+const ProjectType=new GraphQLObjectType({
+    name:'Project',
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString},
+        description:{type:GraphQLString},
+        status:{type:GraphQLString},
+        client:{
+            type:ClientType,
+            resolve(parent,args){
+                return clients.find(client=>client.id === parent.clientId)
+            }
+        }
+    })
+})
+
 
 const RootQuery =new GraphQLObjectType({
 name:'RootQueryType',
 fields:{
+
+    //PROJECT//
+
+    projects:{
+        type: new GraphQLList(ProjectType),
+        resolve(parent,args){
+            return projects;
+        }
+    },
+    project:{
+        type:ProjectType,
+        args:{id:{type:GraphQLID}},
+        resolve(parent,args){
+            return projects.find((project)=>project.id===args.id)
+        },
+    },
+//==========================//
+
+    //CLINET//
+
+    clients:{
+        type: new GraphQLList(ClientType),
+        resolve(parent,args){
+            return clients;
+        }
+    },
     client:{
         type:ClientType,
         args:{id:{type:GraphQLID}},
         resolve(parent,args){
-            return clients.find(client=>client.id===args.id)
+            return clients.find((client)=>client.id===args.id)
         },
     },
 },
+
+// ========================//
 })
 
 module.exports = new GraphQLSchema({
