1b23cd2de88bc01631e18e36f171f332bb36abf2
ananthus9746 <ananthus.ann@gmail.com>
2023-04-03 15:05:39 +0530

    initial

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+const {graphqlHTTP}=require('express-graphql');
+const schema=require('./schema/schema')
+const port =process.env.PORT||5000
+const app =express()
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:true
+}))
+
+app.listen(port,console.log(`Server runing on port : ${port}`))
